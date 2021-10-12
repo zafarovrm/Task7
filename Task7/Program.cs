0b1e2b51a2fb9b2c50408e7c0cb48117fe3fdba8
Руслан Зафаров <zafarovrm@gmail.com>
2021-10-12 21:35:56 +0300

    Добавьте файлы проекта.

---
+++ Task7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1ю сторону 1-го треугольника");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2ю сторону 1-го треугольника");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 3ю сторону 1-го треугольника");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double S;
+            GetParam(a, b, c, out S);
+            double S1 = S;
+            Console.WriteLine("Площадь 1го треугольника равна {0}", S1);
+            Console.WriteLine();
+            Console.WriteLine("Введите 1ю сторону 2-го треугольника");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2ю сторону 2-го треугольника");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 3ю сторону 2-го треугольника");
+            c = Convert.ToInt32(Console.ReadLine());
+            GetParam(a, b, c, out S);
+            double S2 = S;
+            Console.WriteLine("Площадь 2го треугольника равна {0}", S2);
+            Console.WriteLine();
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь 1го треугольника больше");
+            }
+            if (S1 < S2)
+            {
+                Console.WriteLine("Площадь 2го треугольника больше");
+            }
+            if (S1 == S2)
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+        static void GetParam(int a, int b, int c, out double S)
+        {
+            double p = (a + b + c) / 2;
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
---
+++ Task7_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S,V;
+            GetParam(a, out S, out V);            
+            Console.WriteLine("Площадь поверхности куба равна {0}", S);
+            Console.WriteLine("Обьем куба равен {0}", V);
+            Console.WriteLine();
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+        static void GetParam(int a, out double S, out double V)
+        {
+            S = 6 * Math.Pow(a, 2.00);
+            V = Math.Pow(a, 3.00);
+        }
+    }
+}
